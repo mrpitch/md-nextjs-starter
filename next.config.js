e070f88aba2bfac8ebf61254b339c62d9fe7689c
mrpitch <mrpitch@outlook.com>
2022-04-21 16:27:21 +0200

    chore: initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+     locales: ['en', 'nl'],
+     defaultLocale: 'en',
+     localeDetection: false,
+   },
 }
 
 module.exports = nextConfig
