ef3717e7853f96f118808c770e6e02aa0001b8c1
mrpitch <mrpitch@outlook.com>
2022-04-22 12:46:38 +0200

    fix: add tailwind.config, move pages to src

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  mode: "jit",
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.indigo,
+        secondary: colors.orange,
+        gray: colors.neutral,
+      },
+      fontFamily: {
+        sans: ["Graphik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+  ],
+};
